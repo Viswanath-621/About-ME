12e01918df65270308c41790ab0de4e39a114976
Viswanath Bodapati <viswanathbodapati@gmail.com>
2022-10-29 01:26:25 +0530

    Updates

---
+++ precache-manifest.c493bc4b11e38a03d7de92bfc4dc5b72.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56917cd321c67ae5ad484669c38fd7d2",
+    "url": "/About-Me/index.html"
+  },
+  {
+    "revision": "83649d086b96915a915d",
+    "url": "/About-Me/static/css/3.6a7e93e9.chunk.css"
+  },
+  {
+    "revision": "b5cac60b119e0042df1d",
+    "url": "/About-Me/static/css/4.8840eb12.chunk.css"
+  },
+  {
+    "revision": "82da6062acaac0aa77cb",
+    "url": "/About-Me/static/css/main.463cb0b6.chunk.css"
+  },
+  {
+    "revision": "5b83092543131c6d9929",
+    "url": "/About-Me/static/js/2.beeaec71.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/About-Me/static/js/2.beeaec71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83649d086b96915a915d",
+    "url": "/About-Me/static/js/3.44f9c565.chunk.js"
+  },
+  {
+    "revision": "b5cac60b119e0042df1d",
+    "url": "/About-Me/static/js/4.b95addaa.chunk.js"
+  },
+  {
+    "revision": "82da6062acaac0aa77cb",
+    "url": "/About-Me/static/js/main.dd0e6839.chunk.js"
+  },
+  {
+    "revision": "277dd3148595bd36a51f",
+    "url": "/About-Me/static/js/runtime-main.fc122f30.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/About-Me/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "e5eae4ae69e73f2eb848492b61f7c981",
+    "url": "/About-Me/static/media/Cisco-logo.e5eae4ae.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/About-Me/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f68cb7d4a4576a944e59114cf0016942",
+    "url": "/About-Me/static/media/Waterpro.f68cb7d4.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/About-Me/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/About-Me/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "5d6e7fd833ce260469f55c209de2e4f7",
+    "url": "/About-Me/static/media/gfg-logo.5d6e7fd8.jpg"
+  },
+  {
+    "revision": "cbd434148da086ff42f5a8266c11142b",
+    "url": "/About-Me/static/media/google_cloud.cbd43414.png"
+  },
+  {
+    "revision": "33626a84917b3ef5e6753093c369b3ec",
+    "url": "/About-Me/static/media/isplogo.33626a84.png"
+  },
+  {
+    "revision": "252da64c86ece39b0ea495b0f09dd37f",
+    "url": "/About-Me/static/media/lfjclogo.252da64c.png"
+  },
+  {
+    "revision": "f0dfd79dc74ff2a20c9651681ac39864",
+    "url": "/About-Me/static/media/lgmlogo.f0dfd79d.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/About-Me/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/About-Me/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/About-Me/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "aa25192769f20609a9de3a22e8d4bb8e",
+    "url": "/About-Me/static/media/terror.aa251927.jpg"
+  },
+  {
+    "revision": "59cd877ee251d554a8c9e4bb3dc5b0a0",
+    "url": "/About-Me/static/media/vrseclogo.59cd877e.png"
+  }
+]);
